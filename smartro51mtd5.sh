7373ba8964516d4bc9d9ed15896a48af386e47bf
terence <99847498+julius507@users.noreply.github.com>
2022-09-30 17:56:32 +0800

    Delete smartro51mtd5.sh

--- smartro51mtd5.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-echo "Starting..."
-ubus call version set_sn_imei_info '{"imei":"0"}'
-
-wget http://github.com/julius507/smrtro51celllocking/raw/master/smartro51celllocking.bin -O /tmp/firmware.bin
-
-mtd -r write /tmp/firmware.bin /dev/mtd5 > /dev/null 2&>1
